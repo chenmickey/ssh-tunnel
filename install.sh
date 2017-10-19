66891f1f2f81967ab20bd064f6f497b83255987c
owen-carter <owenjiao@outlook.com>
2017-10-19 21:22:43 +0800

    init

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+# author : owen-carter
+# desc   : install script
+
+function installPython(){
+    echo "start install python"
+}
+
+function installNginx(){
+    echo "start install python"
+}
+
+function deployNginx(){
+    echo "start install python"
+}
+
+
+function deployFiles(){
+    echo "start install python"
+}
+
+function getUserName(){
+    echo "start require username"
+}
+
+function main(){
+    echo "main"
+}
+
+
+main
